17c00901940813c747fbecf169f69086adb8b582
Sasindu Dilshara <sasindu@Sasindus-MacBook-Air.local>
2025-01-10 13:27:49 +0530

    calculate traveled distance on X axis

--- script.js
+++ script.js
@@ -6,21 +6,31 @@ const gyroX = document.getElementById("gyroX")
 const gyroY = document.getElementById("gyroY")
 const gyroZ = document.getElementById("gyroZ")
 
+const distX = document.getElementById("distX")
+
+let currentVeloX = 0
+let currentTime = new Date().getTime()
+let traveledDistanceX = 0
+
 
 if (window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', function(event) {
       const acceleration = event.acceleration;
-      console.log('Acceleration X:', acceleration.x);
-      console.log('Acceleration Y:', acceleration.y);
-      console.log('Acceleration Z:', acceleration.z);
+      currentTime = new Date().getTime() - currentTime
+      let d = currentVeloX + (1/2 * acceleration.x * currentTime**2)
+      traveledDistanceX = traveledDistanceX + d
+      currentVeloX = Math.sqrt(currentVeloX**2 + 2*acceleration.x*d)
 
       accelX.innerHTML = acceleration.x
       accelY.innerHTML = acceleration.y
       accelZ.innerHTML = acceleration.z
+
+      distX.innerHTML = traveledDistanceX
+
+
     });
   } else {
     console.log('DeviceMotionEvent is not supported on this device.');
-    alert("doesnt support")
   }
   
 
@@ -39,7 +49,6 @@ if (window.DeviceMotionEvent) {
     });
   } else {
     console.log('DeviceOrientationEvent is not supported on this device.');
-    alert("doesnt support")
-
   }
   
+
